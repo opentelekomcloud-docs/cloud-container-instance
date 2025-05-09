586356b094daf1c8e7ef7e1aa8ee51f43e19480c
Sebastian Gode <sebastian.gode@telekom.de>
2025-05-09 12:17:15 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -38,7 +38,9 @@ otcdocs_doc_type = 'umn'
 otcdocs_service_category = 'container'
 otcdocs_service_title = 'Cloud Container Instance'
 otcdocs_service_type = 'cci'
+otcdocs_cloud_environment = 'eu_de'
 otcdocs_search_environment = 'hc_de'
+otcdocs_search_index = 'search_index_de'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
